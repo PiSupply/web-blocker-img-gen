4cec9fe0d7034555a35500740cb97bba405014b1
Ryan Walmsley <ryanteck@gmail.com>
2020-03-06 22:02:04 +0000

    And finish up

---
+++ stage4/05-pihole/00-run.sh
@@ -0,0 +1,3 @@
+install -m 644 files/logo.png	"${ROOTFS_DIR}/home/adblock/Pictures/"
+install -m 644 files/pihole_install.sh "${ROOTFS_DIR}/home/adblock/Documents/"
+install -m 644 files/PiHole.desktop "${ROOTFS_DIR}/home/adblock/Desktop/"
---
+++ stage4/05-pihole/files/pihole_install.sh
@@ -0,0 +1,4 @@
+
+#!/usr/bin/env bash
+
+curl -sSL https://install.pi-hole.net | bash
